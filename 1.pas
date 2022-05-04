0c331ea7ff667dc8bec0dc54cd235bf8d72b0ea3
schprojectool <97957558+schprojectool@users.noreply.github.com>
2022-05-05 00:57:58 +0500

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,17 @@
+﻿uses graphABC;
+VAR
+x,y:integer;
+
+BEGIN
+x:=0;
+y:=0;
+for var i:=1 to 3 do begin
+  for var j:=1 to 5 do begin
+    {залить рандомным цветом}
+    rectangle(x,y,x + 213, y + 96);
+    y:=y+96;
+  end;
+x:=x+213;
+y:=0;
+end
+END.
---
+++ 2.pas
@@ -0,0 +1,23 @@
+﻿uses graphABC;
+
+procedure arrow(x,y:integer);
+begin
+Line(x,y,x,y+70);
+Line(x,y,x-20,y);
+Line(x+10,y,x+10,y+70);
+Line(x+10,y,x+30,y);
+Line(x+6,y-30,x-20,y);
+Line(x+6,y-30,x+30,y);
+Line(x,y+70,x+10,y+70);
+end;
+
+BEGIN
+Line(220,0,220,480);
+Line(420,0,420,480);
+for var i:=1 to 7 do begin
+arrow(random(140) + 20, random(400)+30); 
+end;
+for var i:=1 to 8 do begin
+arrow(random(100) + 440, random(400)+30); 
+end;
+END.
